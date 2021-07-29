7b395b7ff128922c1c6c867b1cbad1ec47b14aa3
ibaki hongo <redroom013@gmail.com>
2021-07-30 04:39:52 +0900

    Facilityカラムのマーグレーション作成および実行

---
+++ db/migrate/20210729193426_create_facilities.rb
@@ -0,0 +1,17 @@
+class CreateFacilities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :facilities do |t|
+      t.string :name,  null: false, index: true
+      t.string :prefecture, null: false, index: true
+      t.string :address, null: false, index: true
+      t.string :homepage
+      t.string :business_hours
+      t.string :holiday
+      t.string :fee
+      t.string :payment
+      t.text :image
+
+      t.timestamps
+    end
+  end
+end
