0ecc5ca80e4bf8cd1e384be8127510693ef6663c
ibaki hongo <redroom013@gmail.com>
2021-07-30 11:39:37 +0900

    Review.create機能を追加 #9

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path,notice:"施設情報を登録しました"
+    else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:sex, :dry, :light, :wide, :comment,:user_id, :facility_id)
+  end
 end
