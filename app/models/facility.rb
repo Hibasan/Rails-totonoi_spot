32106913abe539dbe27c84f73070649bcf8a21ef
ibaki hongo <redroom013@gmail.com>
2021-08-03 14:17:11 +0900

    Facilityにネストした形でWaterBathテーブルのCRUD機能作成

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -3,4 +3,5 @@ class Facility < ApplicationRecord
   has_many :saunas, dependent: :destroy
   has_many :water_baths, dependent: :destroy
   accepts_nested_attributes_for :saunas, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :water_baths, allow_destroy: true, reject_if: :all_blank
 end
