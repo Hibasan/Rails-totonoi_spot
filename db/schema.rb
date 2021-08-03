41d00b750687de74a7e853e09c94a90cf105d448
ibaki hongo <redroom013@gmail.com>
2021-08-03 11:14:40 +0900

    saunaテーブル作成およびFacilityとの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_013246) do
+ActiveRecord::Schema.define(version: 2021_08_03_020449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 2021_07_30_013246) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
+  create_table "saunas", force: :cascade do |t|
+    t.integer "sex"
+    t.integer "temperature"
+    t.integer "intern"
+    t.string "comment"
+    t.bigint "facility_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["facility_id"], name: "index_saunas_on_facility_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -72,4 +83,5 @@ ActiveRecord::Schema.define(version: 2021_07_30_013246) do
 
   add_foreign_key "reviews", "facilities"
   add_foreign_key "reviews", "users"
+  add_foreign_key "saunas", "facilities"
 end
