40baf24a741ee3363e5db8bb3590fc7b9f1c273d
ibaki hongo <redroom013@gmail.com>
2021-08-03 15:12:36 +0900

    RestAreaのCRUD機能追加 #15

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -2,9 +2,9 @@ class Facility < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :saunas, dependent: :destroy
   has_many :water_baths, dependent: :destroy
-  belongs_to :rest_area
+  has_many :rest_areas, dependent: :destroy
   accepts_nested_attributes_for :saunas, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :water_baths, allow_destroy: true, reject_if: :all_blank
-  accepts_nested_attributes_for :rest_area, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :rest_areas, allow_destroy: true, reject_if: :all_blank
 
 end
