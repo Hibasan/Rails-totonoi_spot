c0be4753a4a34283fa4c8ae3f8318eb54de27758
ibaki hongo <redroom013@gmail.com>
2021-08-03 14:04:20 +0900

    water_bathテーブル作成およびFacilityとの関連付け

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,5 +1,6 @@
 class Facility < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :saunas, dependent: :destroy
+  has_many :water_baths, dependent: :destroy
   accepts_nested_attributes_for :saunas, allow_destroy: true, reject_if: :all_blank
 end
---
+++ test/models/water_bath_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WaterBathTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
