1a0f7253f77d51bec07551f13df514fa5b57af12
ibaki hongo <redroom013@gmail.com>
2021-08-14 18:34:12 +0900

    Ajaxを使用した施設お気に入り機能実装

--- app/helpers/favorite_facilities_helper.rb
+++ app/helpers/favorite_facilities_helper.rb
@@ -1,2 +1,10 @@
 module FavoriteFacilitiesHelper
+
+  def favorite_facility?
+    if user_signed_in?
+      @favorite = current_user.favorite_facilities.find_by(facility_id: @facility.id)
+    end
+    @favorite.present?
+  end
+
 end
