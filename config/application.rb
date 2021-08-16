d377b8cbb6f2bdccbf4fb43c48baaea48edb180a
ibaki hongo <redroom013@gmail.com>
2021-08-16 16:42:33 +0900

    Review.newページのレイアウト作成 #63

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TotoSupo
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.assets.initialize_on_precompile = false
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
