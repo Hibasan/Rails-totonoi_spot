7f19a1f7439f608c09be65cef2a1dfff3b53038b
ibaki hongo <redroom013@gmail.com>
2021-08-18 15:40:13 +0900

    Review.edit/destroyコントローラ作成 #75

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,25 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to facility_path(@review.facility_id),notice:"評価を編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to facility_path(@review.facility_id),notice:"評価を削除しました"
+  end
+
   private
   def review_params
     params.require(:review).permit(:sex, :dry, :light, :wide, :comment,:user_id, :facility_id)
