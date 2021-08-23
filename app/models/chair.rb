6c5914081b9bf082e4eab04e9e41ea93271387f8
ibaki hongo <redroom013@gmail.com>
2021-08-23 20:11:28 +0900

    休憩エリアのバリデーション追加(1以上入力規制) #88

--- app/models/chair.rb
+++ app/models/chair.rb
@@ -1,6 +1,11 @@
 class Chair < ApplicationRecord
   validates :sex_id, presence: true
   validates :rest_area_id, presence: true
+  validates :bath, inclusion: { in: 1.. }
+  validates :deck, inclusion: { in: 1.. }
+  validates :relax, inclusion: { in: 1.. }
+  validates :bench, inclusion: { in: 1.. }
+  validates :bench_non_backrest, inclusion: { in: 1.. }
   belongs_to :facility
   belongs_to :sex
   belongs_to :rest_area
