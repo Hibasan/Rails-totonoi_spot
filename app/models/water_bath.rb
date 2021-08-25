7742640a72462617638743b856f587d8a8e540c1
ibaki hongo <redroom013@gmail.com>
2021-08-25 12:31:07 +0900

    エラーメッセージ微修正

--- app/models/water_bath.rb
+++ app/models/water_bath.rb
@@ -1,7 +1,7 @@
 class WaterBath < ApplicationRecord
   validates :sex_id, presence: {message: "性別を選択してください"}
-  validates :temperature, presence: {message: "正しい数字を入力してください"}
-  validates :intern, inclusion: { in: 1..99 ,message: "正しい数値を数字を入力してください (1〜99)"}
+  validates :temperature, presence: {message: "温度に数字を入力してください"}
+  validates :intern, inclusion: { in: 1..99 ,message: "収容人数に正しい数値を数字を入力してください (1〜99)"}
   belongs_to :facility
   belongs_to :sex
 end
